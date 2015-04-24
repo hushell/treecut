e4c6c85efe9dd5b4720ad97a620a32f6af161c48
hushell <dom343@gmail.com>
2015-04-25 01:05:18 +0200

    generate subject data

--- generate_subject_data.m
+++ generate_subject_data.m
@@ -1,7 +1,8 @@
+function generate_subject_data(subjects)
 %
+%subjects = [1105, 1123];
 
 iids_train = load(fullfile('data/iids_train.txt'));
-subjects = [1105, 1123];
 
 all_iids = [];
 all_segs = {};
@@ -21,4 +22,4 @@ for i = 1:numel(iids_train)
     %save(['data/groundTruth/train_uid/' num2str(iid) '.mat'], 'segs', 'uids'); 
 end
 
-save('data/gt_1105_1123.mat', 'all_iids', 'all_segs', 'all_uids');
+save(['data/gt_' num2str(subjects) '.mat'], 'all_iids', 'all_segs', 'all_uids');
